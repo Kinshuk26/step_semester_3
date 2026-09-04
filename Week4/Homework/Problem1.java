2c9201aade306a045b6592de41afbbb40a900f53
Kinshuk26 <kinshukj26@gmail.com>
2026-09-04 22:11:26 +0530

    Added Week 4 Homework

---
+++ Week4/Homework/Problem1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Problem1 {
+    static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int[] result = productExceptSelf(nums);
+        System.out.println(Arrays.toString(result));
+        sc.close();
+    }
+}
